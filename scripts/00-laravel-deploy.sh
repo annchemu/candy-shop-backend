1fb5ed704a17711bab5d564bafb2d7c5c4c32445
Ann Chemutai <annchemu@gmail.com>
2024-07-24 05:52:45 +0300

    laravel script update

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,12 +3,6 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "Running migrations..."
-php artisan migrate --force
-
-echo "Seeding database"
-php artisan db:seed
-
 echo "generating application key..."
 php artisan key:generate --show
 
@@ -18,3 +12,8 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Running migrations..."
+php artisan migrate --force
+
+echo "Seeding database"
+php artisan db:seed --force
